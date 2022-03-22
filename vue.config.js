1bbdec69aa604b4ec3b2563d139937bc00d23f3d
KyleBing <kylebing@163.com>
2022-03-22 22:36:00 +0800

    添加自动打包工具

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,33 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+
 module.exports = {
     publicPath: './',
     productionSourceMap: false, // 不产出 .map 文件
 
     // outputDir: '../voila',
 
+
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            let plugins = []
+            plugins.push(
+                new FileManagerPlugin({
+                    events: {
+                        onEnd: {
+                            mkdir: ['./archive'], // 新建 ./archive 目录
+                            archive: [
+                                // 打包 压缩包中不带 dist 外壳
+                                {
+                                    source: './dist/',
+                                    destination: './archive/voila.zip',
+                                    options: {}
+                                },
+                            ]
+                        }
+                    }
+                })
+            )
+            config.plugins = config.plugins.concat(plugins) // 将新建的 plugin 添加到原 config 中的 plugin 中
+        }
+    }
 }
